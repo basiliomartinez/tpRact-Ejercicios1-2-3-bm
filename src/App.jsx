2c1628e00beffe8ba0bf9a72dec315a85f66775a
basiliomartinez <basilioezequielmartinez@gmail.com>
2025-10-28 15:21:55 -0300

    Resolucion ejercicio 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,36 @@
-// =====================
-// Componente principal App
-// =====================
-
-// Importamos nuestro componente HelloWorld
 import Helloworld from "./components/ej1/Helloworld";
+import Hellofriend from "./components/ej2/Hellofriend";
 
 const App = () => {
-  return (
-    <>
-      <header className="py-5 text-center">
-        <h1 className="display-6">Ejercicio 1 - React ⚛️</h1>
-      </header>
+    return (
+        <>
+            <header className="py-3 text-center">
+                <h1 className="display-6">Ejercicio 1 - React ⚛️</h1>
+            </header>
 
-      <main className="container">
-        {/* Renderizamos el componente HelloWorld */}
-        <Helloworld />
-      </main>
-    </>
-  );
+            <main className="container">
+
+                {/* =======================
+                   EJERCICIO 1
+                ======================= */}
+                <section className="mb-4">
+                    <Helloworld />
+                </section>
+
+                {/* =======================
+                   EJERCICIO 2
+                ======================= */}
+                <section className="mb-5">
+                    <h2 className="h4 alert alert-success mb-3">
+                        Ejercicio 2: "Hello my friend!"
+                    </h2>
+
+                    <Hellofriend mensaje="Hello my friend!" />
+                </section>
+
+            </main>
+        </>
+    );
 };
 
 export default App;
